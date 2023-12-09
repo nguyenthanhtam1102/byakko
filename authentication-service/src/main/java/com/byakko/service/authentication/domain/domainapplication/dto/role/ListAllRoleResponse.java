b28f0a3c7e24b8a555f4381bb0c0dbdac0edab83
nguyenthanhtam1102 <thanhtam1102@hotmail.com>
2023-12-09 20:39:23 +0700

    add jar.enable

--- authentication-service/src/main/java/com/byakko/service/authentication/domain/domainapplication/dto/role/ListAllRoleResponse.java
+++ authentication-service/src/main/java/com/byakko/service/authentication/domain/domainapplication/dto/role/ListAllRoleResponse.java
@@ -3,8 +3,10 @@ package com.byakko.service.authentication.domain.domainapplication.dto.role;
 import com.byakko.common.application.dto.ListAllResponse;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.experimental.SuperBuilder;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @AllArgsConstructor
 @SuperBuilder
