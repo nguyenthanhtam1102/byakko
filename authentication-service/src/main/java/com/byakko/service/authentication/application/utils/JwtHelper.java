c2406f4a85f6de7434877e2b6b3253faa5bf675f
nguyenthanhtam1102 <thanhtam1102@hotmail.com>
2023-12-09 20:51:28 +0700

    Update JJWT9.x

--- authentication-service/src/main/java/com/byakko/service/authentication/application/utils/JwtHelper.java
+++ authentication-service/src/main/java/com/byakko/service/authentication/application/utils/JwtHelper.java
@@ -3,12 +3,13 @@ package com.byakko.service.authentication.application.utils;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 
@@ -21,18 +22,22 @@ public class JwtHelper {
         return keyGenerator.generateKey();
     }
     public String generateToken(String data) {
-        Key key = Keys.hmacShaKeyFor(secret.getBytes());
+        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(secret);
+        Key key = new SecretKeySpec(apiKeySecretBytes, SignatureAlgorithm.HS256.getJcaName());
+
         String token = Jwts.builder()
                 .setSubject(data)
-                .signWith(key, SignatureAlgorithm.HS256)
+                .signWith(SignatureAlgorithm.HS256, key)
                 .compact();
         return token;
     }
 
     public Claims decodeToken(String token){
-        Claims claims =  Jwts.parserBuilder()
-                .setSigningKey(Keys.hmacShaKeyFor(secret.getBytes()))
-                .build()
+        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(secret);
+        Key key = new SecretKeySpec(apiKeySecretBytes, SignatureAlgorithm.HS256.getJcaName());
+
+        Claims claims =  Jwts.parser()
+                .setSigningKey(key)
                 .parseClaimsJws(token)
                 .getBody();
         return claims;
